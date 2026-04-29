c325bed8e263ec7cfdd4347b3c3d6405558fbab4
Ilayda-Bozkurt <ilaydabozkurt@posta.mu.edu.tr>
2026-04-29 12:49:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const apiKey = "aa83e19e"; 
+const searchBtn = document.getElementById('searchBtn');
+const searchInput = document.getElementById('searchInput');
+const resultsDiv = document.getElementById('results');
+
+searchBtn.addEventListener('click', () => {
+    const movieName = searchInput.value;
+    if (movieName) {
+        fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${movieName}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.Response === "True") {
+                    displayMovies(data.Search);
+                } else {
+                    resultsDiv.innerHTML = "<p>Movie not found!</p>";
+                }
+            });
+    }
+});
+
+function displayMovies(movies) {
+    resultsDiv.innerHTML = ""; // Önceki sonuçları temizle
+    movies.forEach(movie => {
+        const movieCard = `
+            <div class="movie-card">
+                <img src="${movie.Poster}" alt="${movie.Title}">
+                <h3>${movie.Title}</h3>
+                <p>${movie.Year}</p>
+            </div>
+        `;
+        resultsDiv.innerHTML += movieCard;
+    });
+}
